9a7fba498ff7dfd3e6c37e3dd265dc863ae65e1f
stephen muiru <stevembugua26@gmail.com>
2026-05-19 17:13:10 +0300

    worked on the controller functions

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,2 +1,22 @@
+const pool = require("./pool")
 
+async function getItems(req, res) {
+ try {
+   const {rows} = await pool.query("SELECT * FROM items JOIN categories ON items.category_id = categories.id") 
+   res.render("index", {items : rows})
+ } catch(err) {
+   console.error("Something went wrong", err)
+ }  
+}
 
+async function getItemsByCategory(req, res) {
+  try {
+    const {id} = req.params
+    const {rows} = await pool.query("SELECT * FROM items JOIN categories ON items.category_id = categories.id WHERE items.category_id = $1", [id])
+     res.render("index", {category: rows})
+  } catch(err) {
+     console.error("Something went wrong", err)
+  }
+}
+
+module.exports = { getItems, getItemsByCategory }
